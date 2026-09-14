845502205b89c1ac01e606197b07300873c47b7e
Kofitto404 <dad.gonzalez@duoc.cl>
2026-09-14 14:56:46 -0300

    esqueleto amplify

---
+++ src/aws-exports.js
@@ -0,0 +1,21 @@
+const awsconfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: '', // Reemplaza con tu User Pool ID
+      userPoolClientId: '', // Reemplaza con tu App Client ID
+      signUpVerificationMethod: 'code',
+      loginWith: {
+        email: true
+      }
+    }
+  },
+  API: {
+    REST: {
+      'api-banco': {
+        endpoint: 'https://qw81piu9tk.execute-api.us-east-1.amazonaws.com'
+      }
+    }
+  }
+};
+
+export default awsconfig;
